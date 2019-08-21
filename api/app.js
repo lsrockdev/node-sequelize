91a771826b5bea866fadaf66b74bc8551b7512ff
Liu Lei <greenborn101@gmail.com>
2019-08-21 06:54:36 -0700

    updated code structure,  install cors, nodemon, cross-env

--- api/app.js
+++ api/app.js
@@ -2,25 +2,33 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const Sequelize = require('sequelize');
+const cors = require('cors');
+const http = require('http');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
+const server = http.Server(app);
+const config = require('../config/');
+const environment = process.env.NODE_ENV;
+const dbService = require('./services/db.service');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
+const DB = dbService(environment, config.migrate).start();
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-const sequelize = new Sequelize('tapster', 'root', '12345678', {
-    host: 'localhost',
-    dialect: 'mysql'
-});
+server.listen(config.port, () => {
+    if (environment !== 'production' &&
+      environment !== 'development' &&
+      environment !== 'testing'
+    ) {
+      console.error(`NODE_ENV is set to ${environment}, but only production and development are valid.`);
+      process.exit(1);
+    }
+    return DB;
+  });
   
 module.exports = app;
---
+++ config/connection.js
@@ -0,0 +1,30 @@
+const development = {
+    database: 'Tapster',
+    username: 'root',
+    password: '12345678',
+    host: 'localhost',
+    dialect: 'mysql',
+  };
+  
+  const testing = {
+    database: 'databasename',
+    username: 'username',
+    password: 'password',
+    host: 'localhost',
+    dialect: 'mysql',
+  };
+  
+  const production = {
+    database: process.env.DB_NAME,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    host: process.env.DB_HOST || 'localhost',
+    dialect: 'mysql',
+  };
+  
+  module.exports = {
+    development,
+    testing,
+    production,
+  };
+  
