124ae2e0a3d7129d51624edca25bbe2fded24d81
Liu Lei <greenborn101@gmail.com>
2019-08-22 07:38:22 -0700

    fixed a port issue

--- config/index.js
+++ config/index.js
@@ -3,7 +3,7 @@ const routes = require("./routes/index");
 const config = {
   migrate: true,
   routes,
-  port: process.env.PORT || "2017"
+  port: process.env.PORT || "3000"
 };
 
 module.exports = config;
