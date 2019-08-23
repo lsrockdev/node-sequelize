e0664b8d85a323eea199e6d1a9005575cbe77a7d
Liu Lei <greenborn101@gmail.com>
2019-08-23 08:17:21 -0700

    updated development config

--- config/config.js
+++ config/config.js
@@ -1,12 +1,11 @@
 require("dotenv").config(); // this is important!
 module.exports = {
   development: {
-    username: "root",
-    password: "12345678",
-    database: "Tapster",
-    host: "127.0.0.1",
-    dialect: "mysql",
-    operatorsAliases: false
+    database: process.env.DB_NAME,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    host: "localhost",
+    dialect: "mysql"
   },
   test: {
     database: process.env.DB_NAME,
