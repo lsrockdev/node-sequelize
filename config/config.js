ce820f1868b6580f7bf06ed8ae0f032a96024041
Liu Lei <greenborn101@gmail.com>
2019-08-24 11:28:56 -0700

    Puts db config.js back to using env variables instead of hard coded values

--- config/config.js
+++ config/config.js
@@ -1,17 +1,17 @@
 require("dotenv").config(); // this is important!
 module.exports = {
   development: {
-    username: "root",
-    password: "12345678",
-    database: "Tapster",
+    database: process.env.DB_NAME,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
     host: "127.0.0.1",
     dialect: "mysql",
     operatorsAliases: false
   },
   test: {
-    database: process.env.DB_NAME,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
+    database: process.env.DB_TEST_NAME,
+    username: process.env.DB_TEST_USER,
+    password: process.env.DB_TEST_PASS,
     host: process.env.DB_HOST || "localhost",
     dialect: "mysql"
   },
