c3a7be8b7a50fff98ddff93966917a39f7245acf
Liu Lei <greenborn101@gmail.com>
2019-08-25 11:55:10 -0700

    added UserLocation model

---
+++ migrations/20190825185229-create-user-location.js
@@ -0,0 +1,54 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('UserLocations', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      address1: {
+        type: Sequelize.STRING
+      },
+      address2: {
+        type: Sequelize.STRING
+      },
+      address3: {
+        type: Sequelize.STRING
+      },
+      type: {
+        type: Sequelize.INTEGER
+      },
+      latitude: {
+        type: Sequelize.DOUBLE
+      },
+      longitude: {
+        type: Sequelize.DOUBLE
+      },
+      isActive: {
+        type: Sequelize.BOOLEAN
+      },
+      customerId: {
+        type: Sequelize.INTEGER
+      },
+      stateId: {
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('UserLocations');
+  }
+};
---
+++ models/userlocation.js
@@ -0,0 +1,19 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const UserLocation = sequelize.define('UserLocation', {
+    address1: DataTypes.STRING,
+    address2: DataTypes.STRING,
+    address3: DataTypes.STRING,
+    type: DataTypes.INTEGER,
+    latitude: DataTypes.DOUBLE,
+    longitude: DataTypes.DOUBLE,
+    isActive: DataTypes.BOOLEAN,
+    customerId: DataTypes.INTEGER,
+    stateId: DataTypes.INTEGER,
+    userId: DataTypes.INTEGER
+  }, {});
+  UserLocation.associate = function(models) {
+    // associations can be defined here
+  };
+  return UserLocation;
+};
