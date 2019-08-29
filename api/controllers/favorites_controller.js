220326d0804d0b002ccee29051fb1ec1876cdaa6
Liu Lei <greenborn101@gmail.com>
2019-08-29 02:27:59 -0700

    added addOne function in FavoritesController

--- api/controllers/favorites_controller.js
+++ api/controllers/favorites_controller.js
@@ -1,17 +1,36 @@
 const Favorites = require("../../models").Favorites;
+const Category = require("../../models").Category;
+const Customer = require("../../models").Customer;
+
+Customer.hasMany(Favorites, { foreignKey: "customerId" });
+Favorites.belongsTo(Customer, { foreignKey: "customerId" });
+
+Category.hasMany(Favorites, { foreignKey: "categoryId" });
+Favorites.belongsTo(Category, { foreignKey: "categoryId" });
 
 const FavoritesController = () => {
   const getAll = async (req, res) => {
+    const favoriteProducts = await Favorites.findAll({
+      where: {
+        deleted: false || null
+      },
+      include: [Customer, Favorites]
+    });
     return res.status(200).json({
-      message: "testing",
+      favoriteProducts,
+      message: "Favorite product read successful.",
       StatusCode: 1
     });
   };
 
   const addOne = async (req, res) => {
     const { body } = req;
+    const customerId = req.token.id;
     try {
-      const favorite = await Favorites.create(body);
+      await Favorites.create({
+        customerId,
+        ...body
+      });
       return res.status(200).json({
         message: "Favorite Product Added Succesfully",
         StatusCode: 1,
