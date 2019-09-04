b3f81e189d47f184c30b758c06405ba1ad669751
Liu Lei <greenborn101@gmail.com>
2019-09-04 05:40:38 -0700

    fixed a migrate file

--- migrations/20190904093844-add_columns_to_storeuser.js
+++ migrations/20190904093844-add_columns_to_storeuser.js
@@ -1,23 +1,25 @@
-'use strict';
+"use strict";
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
+    return Promise.all([
+      queryInterface.addColumn("StoreUsers", "isDeleted", {
+        type: Sequelize.BOOLEAN
+      }),
+      queryInterface.addColumn("StoreUsers", "isRegistered", {
+        type: Sequelize.BOOLEAN
+      }),
+      queryInterface.addColumn("StoreUsers", "roleId", {
+        type: Sequelize.INTEGER
+      })
+    ]);
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.dropTable('users');
-    */
+    return Promise.all([
+      queryInterface.removeColumn("StoreUsers", "isDeleted"),
+      queryInterface.removeColumn("StoreUsers", "isRegistered"),
+      queryInterface.removeColumn("StoreUsers", "roleId")
+    ]);
   }
 };
