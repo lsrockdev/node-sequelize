1827e937f921fee5ca8d86d047e3b00ce0b9c38c
Liu Lei <greenborn101@gmail.com>
2019-09-04 04:51:38 -0700

    added admin log in api

--- api/controllers/admins_controller.js
+++ api/controllers/admins_controller.js
@@ -1,10 +1,45 @@
+const StoreUser = require("../../models").StoreUser;
+const authService = require("../services/auth.service");
+const bcryptService = require("../services/bcrypt.service");
+
 const AdminController = () => {
-  const getAll = async (req, res) => {
-    return res.status(200).json({ message: "" });
+  const login = async (req, res) => {
+    const { password } = req.body;
+    const email = req.body.email.toLowerCase();
+    if (email && password) {
+      try {
+        let user = await StoreUser.findOne({
+          where: {
+            email: email,
+            roleId: 1
+          }
+        });
+        if (!user) {
+          return res.status(400).json({ msg: "Bad Request: User not found" });
+        }
+        if (bcryptService().comparePassword(password, user.password)) {
+          const token = authService().issue({ id: user.id });
+          return res.status(200).json({
+            message: "Login Success",
+            StatusCode: 1,
+            user,
+            token
+          });
+        }
+        return res.status(401).json({ msg: "Unauthorized" });
+      } catch (err) {
+        console.log(err);
+        return res.status(500).json({ msg: "Internal server error" });
+      }
+    }
+
+    return res
+      .status(400)
+      .json({ msg: "Bad Request: Email or password is wrong" });
   };
 
   return {
-    getAll
+    login
   };
 };
 module.exports = AdminController;
