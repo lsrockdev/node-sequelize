cedcc76e61fde01bbd21756d1c20234b0e195439
Liu Lei <greenborn101@gmail.com>
2019-09-05 20:14:26 -0700

    updateOne, deleteOne methods in category controller

--- api/controllers/category_controller.js
+++ api/controllers/category_controller.js
@@ -61,17 +61,53 @@ const CategoryController = () => {
   };
 
   const updateOne = async (req, res) => {
-    return res.status(200).json({
-      message: "Your category successfully updated.",
-      StatusCode: 1
-    });
+    const { body } = req;
+    try {
+      const category = await Category.findOne({
+        where: {
+          id: body.id
+        }
+      });
+      await updateSizes(category.id, body.categorySizes);
+      delete body.categorySizes;
+      await category.update(body);
+      return res.status(200).json({
+        message: "Your category successfully updated.",
+        StatusCode: 1
+      });
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json({ msg: "Internal server error" });
+    }
   };
 
   const deleteOne = async (req, res) => {
-    return res.status(200).json({
-      message: "Your category successfully deleted.",
-      StatusCode: 1
-    });
+    const { id } = req.body;
+    try {
+      await Category.update({ isDeleted: true }, { where: { id } });
+      return res.status(200).json({
+        message: "Your category successfully deleted.",
+        StatusCode: 1
+      });
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json({ msg: "Internal server error" });
+    }
+  };
+
+  const updateSizes = async (categoryId, sizeIds) => {
+    try {
+      await CatergorySizes.destroy({
+        where: { categoryId }
+      });
+      const categorySizes = sizeIds.map(sizeId => {
+        return { sizeId, categoryId };
+      });
+      await CatergorySizes.bulkCreate(categorySizes);
+      return true;
+    } catch (err) {
+      throw err;
+    }
   };
 
   return { getAll, addOne, updateOne, deleteOne };
