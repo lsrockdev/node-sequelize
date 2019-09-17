15ddae6161555cdf929c0d235937d71b3dd322f5
Liu Lei <greenborn101@gmail.com>
2019-09-17 15:05:55 -0700

    Adds customer getter and setter to driver model to hash password automatically on create/update. Adds passwordString method to bcrypt service to facilitate hashing a pw without user object.

--- models/driver.js
+++ models/driver.js
@@ -1,16 +1,47 @@
-'use strict';
+"use strict";
+
+const bcryptService = require("../api/services/bcrypt.service");
+
+// const hooks = {
+//   beforeCreate(customer) {
+//     customer.password = bcryptService().password(customer);
+//   }
+// };
+
 module.exports = (sequelize, DataTypes) => {
-  const Driver = sequelize.define('Driver', {
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    phone: DataTypes.STRING,
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    isActive: DataTypes.BOOLEAN,
-    lastLogin: DataTypes.DATE,
-    gender: DataTypes.INTEGER,
-    dob: DataTypes.DATE
-  }, {});
+  const Driver = sequelize.define(
+    "Driver",
+    {
+      email: DataTypes.STRING,
+      password: {
+        type: DataTypes.STRING,
+        get: function() {
+          return this.getDataValue("password");
+        },
+        set: function(value) {
+          this.setDataValue(
+            "password",
+            bcryptService().passwordString("welcome")
+          );
+        }
+      },
+      phone: DataTypes.STRING,
+      firstName: DataTypes.STRING,
+      lastName: DataTypes.STRING,
+      isActive: DataTypes.BOOLEAN,
+      lastLogin: DataTypes.DATE,
+      gender: DataTypes.INTEGER,
+      dob: DataTypes.DATE
+    },
+    {}
+  );
+
+  Driver.prototype.toJSON = function() {
+    const values = Object.assign({}, this.get());
+    delete values.password;
+    return values;
+  };
+
   Driver.associate = function(models) {
     // associations can be defined here
   };
