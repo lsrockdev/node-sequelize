15b81cb8762744b1b4a1c4e5fd916d736c42c105
Liu Lei <greenborn101@gmail.com>
2019-09-29 09:06:34 -0700

    updateStore, GetStoreDetails  api for store dashboard

--- config/routes/store/store_auth_routes.js
+++ config/routes/store/store_auth_routes.js
@@ -1,6 +1,10 @@
 var express = require("express");
 var router = express.Router();
 const storeAuthController = require("../../../api/controllers/store/store_auth_controller");
+const storeQueryController = require("../../../api/controllers/store/store_query_controller");
+const storeUpdateController = require("../../../api/controllers/store/store_update_controller");
+
+const authPolicy = require("../../../api/policies/auth.policy");
 
 router.post("/authenticate", function(req, res) {
   return storeAuthController().login(req, res);
@@ -8,6 +12,14 @@ router.post("/authenticate", function(req, res) {
 
 router.post("/register", function(req, res) {
   return storeAuthController().register(req, res);
+});
+
+router.get("/getStoreDetails", authPolicy, function(req, res) {
+  return storeQueryController().getById(req, res);
+});
+
+router.post("/updateStore", authPolicy, function(req, res) {
+  return storeUpdateController().updateOne(req, res);
 });
 
 module.exports = router;
